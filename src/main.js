cd85bd96d28c9c0bbdc57edfb144d3c127ca5dad
fuanxian <329285015@qq.com>
2020-04-16 16:42:01 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import element from 'element-ui'
+import 'node_modules/'
 
 Vue.config.productionTip = false
 
